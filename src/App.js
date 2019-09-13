c8d7afe13eb4952ddae0748c5d709d2a7a04b1ea
bjwellsi <braidenjwells@gmail.com>
2019-09-12 22:44:32 -0600

    started building

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Guards } from './Guards';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Learn how to lightsaber fight here!</h1>
       </header>
+      <main><Guards/></main>
     </div>
   );
 }
---
+++ src/Guards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Guards extends React.Component {
+    render() {
+        let jsx = (
+            <ul>
+            <li>one</li>
+            <li>another</li>
+            </ul>
+        );
+        return jsx;
+    }
+}
